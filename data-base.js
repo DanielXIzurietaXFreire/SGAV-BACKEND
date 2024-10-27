93572e26b4ec4a80957d47f802bd27a7e9668c75
Daniel <kizurieta9883@uta.edu.ec>
2024-10-27 17:42:56 -0500

    Revert "Damos formato al documento"
    This reverts commit f0fbf1c38c03c113450ae66de9198cdbddd77999.

--- data-base.js
+++ data-base.js
@@ -2,8 +2,8 @@ const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
   host: 'localhost',
-  user: 'root',
-  password: '', 
+  user: 'root', // Asegúrate de completar el usuario
+  password: '', // Asegúrate de completar la contraseña
   database: 'vehicle_rental'
 });
 
